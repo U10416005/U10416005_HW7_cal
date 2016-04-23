9d8f97999b4a66e7b56ec7e91dee2881cae0d41f
U10416005 <r3r346@gmail.com>
2016-04-23 18:10:51 +0800

    Write Pane,Button and button content
    Write the pane and the Button to caculate and the content to order

---
+++ CaculatorFunction.java
@@ -0,0 +1,19 @@
+//U10416005ªL«Ø¦t
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class CaculatorFunction extends Application{
+	public void start(Stage primaryStage){
+		Pane pane = new Pane();
+		Button btn[] = new Button[28]
+		String btnIn[] = {"MC", "MR", "MS", "M+", "M-",
+            "¡ö", "CE", "C", "¡Ó", "¡Ô", "7", "8", "9", "¡Ò",
+            "%", "4", "5", "6", "¡Ñ", "1/x","1","2","3","-","=",
+            "0",".","+"};
+	}
+}
