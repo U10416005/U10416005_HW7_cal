c74fc063b04c2d9fd9f210c77bfa7012857c0952
U10416005 <r3r346@gmail.com>
2016-04-23 21:59:19 +0800

    Add button and Text area
    Add buttons and a text area in the pane

--- CaculatorFunction.java
+++ CaculatorFunction.java
@@ -15,9 +15,62 @@ public class CaculatorFunction extends Application{
             "ก๖", "CE", "C", "กำ", "กิ", "7", "8", "9", "กา",
             "%", "4", "5", "6", "กั", "1/x","1","2","3","-","=",
             "0",".","+"};
-		for(int i = 0;i<28;i++){
+		for(int i = 0;i<28;++i){
 			btn[i]=new Button(btnIn[i]);
 		}
+		TextArea cal = new TextArea();
+		pane.getChildren().add(cal);
+        cal.setPrefSize(280, 100);
+        cal.setLayoutX(10);
+        cal.setLayoutY(10);
+        cal.setEditable(false);
+        cal.setText("0");
+		for(int i = 0;i<28;++i){
+			btn[i].setPrefSize(50, 60);
+            if(i<=4) {
+                btn[i].setLayoutX(10+60*i);
+                btn[i].setLayoutY(116);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i>4&&i<=9) {
+                btn[i].setLayoutX(10+60*(i-5));
+                btn[i].setLayoutY(179);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i>9&&i<=14) {
+                btn[i].setLayoutX(10+60*(i-10));
+                btn[i].setLayoutY(242);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i>14&&i<=19) {
+                btn[i].setLayoutX(10+60*(i-15));
+                btn[i].setLayoutY(305);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i>19&&i<=23) {
+                btn[i].setLayoutX(10+60*(i-20));
+                btn[i].setLayoutY(378);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i==24) {
+                btn[i].setLayoutX(10+60*(i-20));
+                btn[i].setLayoutY(378);
+                btn[i].setPrefSize(50, 120);
+                pane.getChildren().add(btn[i]);
+            }
+            else if(i==25) {
+                btn[i].setLayoutX(10+60*(i-25));
+                btn[i].setLayoutY(441);
+                btn[i].setPrefSize(110, 60);
+                pane.getChildren().add(btn[i]);
+            }
+            else {
+                btn[i].setLayoutX(10+60*(i-24));
+                btn[i].setLayoutY(441);
+                pane.getChildren().add(btn[i]);
+            }  
+        }
+	
 		Scene scene = new Scene(pane,300,500);
 		primaryStage.setResizable(false);
         primaryStage.setTitle("U10416005's caculator");
