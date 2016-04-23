ae62417ddb7d3b3aa13de0596fbd14023ae8700a
U10416005 <r3r346@gmail.com>
2016-04-23 22:20:45 +0800

    Write menu bar
    Write the menu bar and add in the pane

--- CaculatorFunction.java
+++ CaculatorFunction.java
@@ -12,17 +12,17 @@ public class CaculatorFunction extends Application{
 		Pane pane = new Pane();
 		Button btn[] = new Button[28];
 		String btnIn[] = {"MC", "MR", "MS", "M+", "M-",
-            "←", "CE", "C", "±", "√", "7", "8", "9", "÷",
-            "%", "4", "5", "6", "×", "1/x","1","2","3","-","=",
+            "<-", "CE", "C", "±", "√", "7", "8", "9", "÷",
+            "%", "4", "5", "6", "*", "1/x","1","2","3","-","=",
             "0",".","+"};
 		for(int i = 0;i<28;++i){
 			btn[i]=new Button(btnIn[i]);
 		}
 		TextArea cal = new TextArea();
 		pane.getChildren().add(cal);
-        cal.setPrefSize(280, 100);
+        cal.setPrefSize(280, 70);
         cal.setLayoutX(10);
-        cal.setLayoutY(10);
+        cal.setLayoutY(30);
         cal.setEditable(false);
         cal.setText("0");
 		for(int i = 0;i<28;++i){
@@ -70,6 +70,32 @@ public class CaculatorFunction extends Application{
                 pane.getChildren().add(btn[i]);
             }  
         }
+		MenuBar bar = new MenuBar();
+		Menu menu1 = new Menu("檢視(V)");
+		Menu menu2 = new Menu("編輯(E)");
+		Menu menu3 = new Menu("說明(H)");
+		MenuItem item1[] = new MenuItem[10];
+		MenuItem item2[] = new MenuItem[3];
+		MenuItem item3[] = new MenuItem[2];
+		String item1Name[] = {"標準型(T)","工程型(S)","程式設計師(P)","統計資料(A)",
+        "歷程紀錄(Y)","數字分位(I)","基本(B)","單位換算(U)","日期計算(D)","工作表(W)"};
+		String item2Name[] = {"複製(C)","貼上(P)","歷程紀錄(H)"};
+		String item3Name[] = {"檢視說明(V)    F1","關於小算盤(A)"};
+		for(int i = 0;i<10;++i){
+			item1[i] = new MenuItem(item1Name[i]);
+            menu1.getItems().add(item1[i]);
+		}
+		for(int i = 0;i<3;++i){
+			item2[i] = new MenuItem(item2Name[i]);
+            menu2.getItems().add(item2[i]);
+		}
+		for(int i = 0;i<2;++i){
+			item3[i] = new MenuItem(item3Name[i]);
+            menu3.getItems().add(item3[i]);
+		}
+		bar.getMenus().addAll(menu1,menu2,menu3);
+		bar.setLayoutX(10);
+		pane.getChildren().add(bar);
 	
 		Scene scene = new Scene(pane,300,500);
 		primaryStage.setResizable(false);
